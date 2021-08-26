13c9f5090a6cac28ac4ce185eec5b14480b745f3
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-08-26 09:38:23 +0300

    password

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Blog, Comment, PhotoProfile
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+app = create_app('product')
 
 manager = Manager(app)
 manager.add_command('server', Server)
